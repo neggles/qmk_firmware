9dcdd18da763b14511bbcb0e22c215d81dbc4c6b
Andrew Powers-Holmes <aholmes@omnom.net>
2023-10-16 22:02:48 +1100

    some keymap tweaks

--- keyboards/clockwork/uconsole/keymaps/default/keymap.c
+++ keyboards/clockwork/uconsole/keymaps/default/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,          KC_Q,      KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,
         KC_QUOT,     KC_A,      KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_ENT,
         KC_LSFT,         KC_Z,      KC_X,       KC_C,       KC_V,      KC_B,        KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_RSFT,
-            OSL(_FN),       KC_LCTL,    KC_LALT,                            KC_SPC,                     KC_RALT,    KC_RCTL,    OSL(_FN)
+            OSL(_FN),       KC_LCTL,    KC_LALT,                            KC_SPC,                     KC_RALT,    KC_RCTL,    TT(_FN)
     ),
     [_FN] = LAYOUT(
                     _______,                _______,                                                            _______,    _______,
@@ -71,19 +71,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,       KC_F7,       KC_F8,       KC_F9,    KC_F10,    KC_DEL,
         KC_CAPS,         _______,   _______,    _______,    _______,    _______,    _______,    KC_PGUP,    KC_INS,     _______,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_END,     _______,    _______,    _______,
-        _______,         _______,   _______,    _______,    _______,    _______,    _______,    _______,    KC_BRID,    KC_BRIU,    _______,
+        _______,         _______,   _______,    _______,    _______,    QK_BOOT,    _______,    _______,    KC_BRID,    KC_BRIU,    _______,
             _______,        _______,    KC_LGUI,                            BL_STEP,                    KC_RGUI,    _______,    _______
     ),
 };
 // clang-format on
 // Override volume up to be volume down when shift is held
 #ifdef KEY_OVERRIDE_ENABLE
-const key_override_t vol_up_override = ko_make_basic(MOD_MASK_SHIFT, KC_VOLU, KC_VOLD);
+const key_override_t shift_vol_up_override = ko_make_basic(MOD_MASK_SHIFT, KC_VOLU, KC_VOLD);
 
 // clang-format off
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &vol_up_override,
-    NULL
+    &shift_vol_up_override,
+    NULL,
 };
 // clang-format on
 #endif
@@ -93,12 +93,12 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
     // Switch trackball mode when layer is turned on
     if (IS_LAYER_ON_STATE(state, _FN)) {
         if (trackball_mode_get() != TRACKBALL_MODE_SCROLL) {
-            uprintf("Trackball: switching to scroll mode\n");
+            dprintf("TB: scroll mode\n");
             trackball_mode_set(TRACKBALL_MODE_SCROLL);
         }
     } else {
         if (trackball_mode_get() != TRACKBALL_MODE_MOUSE) {
-            uprintf("Trackball: switching to mouse mode\n");
+            dprintf("TB: mouse mode\n");
             trackball_mode_set(TRACKBALL_MODE_MOUSE);
         }
     }
@@ -108,17 +108,19 @@ layer_state_t layer_state_set_kb(layer_state_t state) {
 
 void keyboard_post_init_user(void) {
 #ifdef CONSOLE_ENABLE
-    debug_config.enable   = true;
-    debug_config.matrix   = false;
-    debug_config.keyboard = true;
-    debug_config.mouse    = true;
+    debug_config.enable = true;
+    debug_config.mouse  = true;
 #endif
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // If console is enabled, it will print the matrix position and status of each key pressed
 #ifdef CONSOLE_ENABLE
-    dprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+    if (record->event.pressed == 1) {
+        dprintf("KC 0x%04X pressed, C=%02u R=%02u L=0x%02X P=%u T=%5u C=%u I=%u\n", keycode, record->event.key.col, record->event.key.row, layer_state, record->event.pressed, record->event.time, record->tap.count, record->tap.interrupted);
+    } else {
+        dprintf("KC 0x%04X release, C=%02u R=%02u L=0x%02X P=%u T=%5u C=%u I=%u\n", keycode, record->event.key.col, record->event.key.row, layer_state, record->event.pressed, record->event.time, record->tap.count, record->tap.interrupted);
+    }
 #endif
     return true;
 }
